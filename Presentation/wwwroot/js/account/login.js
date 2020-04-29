da95db3666e8bb8fb91c2301de75a1b78fd097e2
MD. SOJIB MIYA <lisancseru@gmail.com>
2020-04-29 21:19:17 +0600

    Updated site.js

--- Presentation/wwwroot/js/account/login.js
+++ Presentation/wwwroot/js/account/login.js
@@ -25,7 +25,7 @@
             axios.post('/api/auth/login', data)
                 .then(function (response) {
                     toastr.success("Login successful!");
-                    localStorage.setItem("token", response.data.access_token);
+                    localStorage.setItem("token", response.data);
                     window.location.href = "/account/profile";
                 })
                 .catch(function (err) {
--- Presentation/wwwroot/js/site.js
+++ Presentation/wwwroot/js/site.js
@@ -1,11 +1,7 @@
-﻿var rootPath;
-
-$(function () {
-    rootPath = window.location.protocol + '//' + window.location.host;
-
+﻿$(function () {
     toastr.options.escapeHtml = true;
     axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-    debugger;
+
     if (localStorage.getItem("token")) {
         axios.defaults.headers.common['Authorization'] = "Bearer " + localStorage.getItem("token");
     }
