2b4302dfcade443467fae04c02212990158ea7e7
MD. SOJIB MIYA <lisancseru@gmail.com>
2020-04-29 20:06:25 +0600

    User Email and Username length updated

---
+++ Infrastructure/Data/Migrations/20200428221756_DbChanges.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class DbChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "UniqueKey_Email",
+                table: "Users",
+                column: "Email");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "UniqueKey_Username",
+                table: "Users",
+                column: "Username");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "UniqueKey_Email",
+                table: "Users");
+
+            migrationBuilder.DropUniqueConstraint(
+                name: "UniqueKey_Username",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 250);
+        }
+    }
+}
